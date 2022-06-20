493c9e1c42a19387763481db33e52c95c71bf2a8
tempoeric <42777871+tempoeric@users.noreply.github.com>
2022-06-20 17:01:13 +0800

    fix: COVID-001 initial commit of cra

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'no-console': 1,
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: { 'prettier/prettier': ['warn', prettierOptions] },
+    },
+  ],
+};
---
+++ src/store/configureStore.ts
@@ -0,0 +1,44 @@
+/**
+ * Create the store with dynamic reducers
+ */
+
+import { configureStore } from '@reduxjs/toolkit';
+import { createInjectorsEnhancer, forceReducerReload } from 'redux-injectors';
+import createSagaMiddleware from 'redux-saga';
+
+import { createReducer } from './reducers';
+import { logger } from '../middlewares';
+
+export function configureAppStore() {
+  const reduxSagaMonitorOptions = {};
+  const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
+  const { run: runSaga } = sagaMiddleware;
+  const active = process.env.NODE_ENV !== 'production';
+
+  // Create the store with saga middleware
+
+  const enhancers = [
+    createInjectorsEnhancer({
+      createReducer,
+      runSaga,
+    }),
+  ];
+
+  const store = configureStore({
+    reducer: createReducer(),
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware().concat(logger(active)),
+    devTools: true,
+    enhancers,
+  });
+
+  // Make reducers hot reloadable, see http://mxs.is/googmo
+  /* istanbul ignore next */
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      forceReducerReload(store);
+    });
+  }
+
+  return store;
+}
